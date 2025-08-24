65d47b3c339810cc3896bd13e79a6f664941d526
Laurentiu Kotlacsik <KotlaSick@gmail.com>
2025-08-24 14:39:40 +0000

    si putem schimba fontul cu "https://fonts.google.com/specimen/DM+Serif+T

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Roboto', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        headline: ['"DM Serif Text"', 'serif'],
         code: ['monospace'],
         mono: ['Anonymous Pro', 'monospace'],
       },
