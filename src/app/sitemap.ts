beafc1869ff47179a6ced6e677867e23e2c8ce5f
Laurentiu Kotlacsik <KotlaSick@gmail.com>
2025-09-04 12:40:25 +0000

    as avea nevoie si de fisierul sitemap.xml, nu?

---
+++ src/app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://mitarmedia.ro';
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/social`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/qr`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ]
+}
